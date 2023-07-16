2063f1626419cffca8e25a47199e3dbc0c082e67
irah rosete <72952642+irahrosete@users.noreply.github.com>
2023-07-16 14:20:04 +1000

    add buttons and inputs and styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,24 @@ import { useState } from 'react'
 import shuffleLogo from './assets/shuffle.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // const [count, setCount] = useState(0)
+  const [names, setNames] = useState([])
+  const [groupSize, setGroupSize] = useState(2)
+
+  const handleNames = (e) => {
+    setNames(e.target.value);
+  }
+
+  const handleGroupSize = (e) => {
+    setGroupSize(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    console.log(groupSize);
+    console.log(names);
+    e.preventDefault();
+  }
 
   return (
     <>
@@ -11,15 +27,46 @@ function App() {
         <img src={shuffleLogo} className="logo" alt="Shuffle logo" />
         <h1 className='font-dosis'>WiT Randomiser</h1>
       </div>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="inputName">
+            <textarea 
+              className='rounded-md' 
+              autoFocus 
+              name="inputName" 
+              id="inputName" 
+              cols="30" 
+              rows="10" 
+              placeholder='Enter each name on a new line...'
+              onChange={handleNames}>
+            </textarea>
+          </label>
+          <label htmlFor="size">
+            How many in a group?
+            <input 
+              className='rounded-md' 
+              type="number" 
+              id="size" 
+              size="3" 
+              defaultValue={groupSize} 
+              onChange={handleGroupSize}/>
+          </label>
+          <button>
+            run
+          </button>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+      <p className="results">Result here.</p>
+      <p className="results">{groupSize}</p>
+      <p className="results">{names}</p>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button> */}
+      <button>download</button>
+      </div>
     </>
   )
 }
